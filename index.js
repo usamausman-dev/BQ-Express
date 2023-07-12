f927f0db448740af2e4140a6adc0e6ee386d632b
Usama Usman <50479258+usamausman-dev@users.noreply.github.com>
2023-07-12 03:48:01 -0700

    Express-Routing

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use('/api', require('./api/users/router'))
+app.use('/api/v1', require('./api/products/router'))
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
